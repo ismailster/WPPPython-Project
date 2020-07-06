847821f6f00d7cff6e7c7c10b6967479fddb3898
ismailster <63072377+ismailster@users.noreply.github.com>
2020-07-06 07:23:49 -0400

    This is the final submission.  Updated third endpoint for text, cleaned up code not being used

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify, request, Response, render_template
-import yfinance as yt
 import pandas as pd
 import requests as rq
-import time
-from time import gmtime, strftime
-from datetime import datetime, timedelta
 from json2html import *
+import json
 
 # "https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"
 fb_main_title = "Python Final Project Submission"
@@ -21,17 +18,6 @@ intro_one = "This server uses a publiclly available Covid-19 API at https://api.
 paragraph_1 = "for a "
 
 API_URL = "https://api.covid19api.com"
-# Today_date = strftime("%Y%m%dT00:00:00Z", gmtime())
-# lookback_days = gmtime()-30
-
-end = gmtime()
-end = strftime("%m/%d/%Y", end)
-end = datetime.strptime(end, "%m/%d/%Y") #string to date
-start = end - timedelta(days=10) # date - days
-
-def Covid_API(state, date="2"):
-	output = rq.get(API_URL)
-	return output
 
 app = Flask(__name__)
 
@@ -47,43 +33,20 @@ def summary():
 	if template == "html":
 		output = json2html.convert(json = summary_back.text)
 		output_type = "html"
-	else:
+	elif template == "json":
 		output =  summary_back.json()
 		output_type = "json (this is the default setting)"
+	elif template == "text":
+		output =  summary_back.text
+		output_type = "text (tis may look like json but it's text)"
+	else:
+		output = "ruh roh!"
+		output_type = "I think you made a mistake"
 
 	return render_template("home.html", 
 		name="Imran", feeling="Great", data=output, 
 		main_title=main_title, title1=base_pg_title01, title2=base_pg_title02, intro_one=intro_one, 
 		output_type=output_type)
-
-# @app.route("/summary.json")
-# def summary_json():
-# 	summary_back = rq.get(f"{API_URL}/summary")
-# 	# output = json2html.convert(json = summary_back.text)
-# 	output = summary_back.json()
-# 	return render_template("home.html", 
-# 		name="Imran", feeling="Great", data=output, 
-# 		main_title=main_title, title1=base_pg_title01, title2=base_pg_title02, intro_one=intro_one
-# 		)
-
-
-@app.route("/allcountries")
-def countries():
-	country_list_back = rq.get(f"{API_URL}/countries")
-	return Response(country_list_back.text)
-
-# @app.route("/country/<country_name>")
-# def one_country(country_name):
-# 	country_list_back = rq.get(f"{API_URL}/country/{country_name}/status/confirmed?from=2020-06-30T00:00:00Z&to=2020-07-01T00:00:00Z")
-# 	# user = request.args.get('user')
-# 	# state = request.args.get('state')
-# 	country_name =  country_name.replace("-", " ")
-# 	country_name = country_name.title()
-# 	state = "Yo yo"
-# 	input = country_list_back.text
-# 	new_input = json2html.convert(json = input)
-# 	return render_template("home.html", name=country_name, feeling=state)
-# 	# return new_input
 
 
 
